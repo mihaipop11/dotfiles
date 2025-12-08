8831318247e2995b48dce72864ed24969124a76c
Mihai Pop <mihaipop11@gmail.com>
2025-12-08 23:26:28 +0200

    nvim: update catppuccino

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,19 +1,52 @@
 return {
   'catppuccin/nvim',
+  lazy = true,
   name = 'catppuccin',
-  priority = 1000,
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require('catppuccin').setup {
-      flavour = 'mocha', -- latte, frappe, macchiato, mocha
-      dim_inactive = {
-        enabled = true, -- Dim inactive windows
-        shade = 'dark', -- Shade color for inactive windows
-        percentage = 0.10, -- Percentage of dimming
+  opts = {
+    lsp_styles = {
+      underlines = {
+        errors = { "undercurl" },
+        hints = { "undercurl" },
+        warnings = { "undercurl" },
+        information = { "undercurl" },
       },
-      auto_integrations = true,
-    }
-
-    vim.cmd.colorscheme 'catppuccin'
-  end,
+    },
+    integrations = {
+      aerial = true,
+      alpha = true,
+      cmp = true,
+      dashboard = true,
+      flash = true,
+      fzf = true,
+      grug_far = true,
+      gitsigns = true,
+      headlines = true,
+      illuminate = true,
+      indent_blankline = { enabled = true },
+      leap = true,
+      lsp_trouble = true,
+      mason = true,
+      mini = true,
+      navic = { enabled = true, custom_bg = "lualine" },
+      neotest = true,
+      neotree = true,
+      noice = true,
+      notify = true,
+      snacks = true,
+      telescope = true,
+      treesitter_context = true,
+      which_key = true,
+    },
+  },
+  specs = {
+    {
+      "akinsho/bufferline.nvim",
+      optional = true,
+      opts = function(_, opts)
+        if (vim.g.colors_name or ""):find("catppuccin") then
+          opts.highlights = require("catppuccin.special.bufferline").get_theme()
+        end
+      end,
+    },
+  },
 }
